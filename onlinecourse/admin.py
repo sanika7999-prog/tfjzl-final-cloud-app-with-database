3bdee3b0a5b05ec93b32caeb6734647555aabc67
sanika7999-prog <sanika7999@gmail.com>
2026-08-30 16:18:55 +0530

    Register admin models and add inline classes

--- onlinecourse/admin.py
+++ onlinecourse/admin.py
@@ -1,4 +1,28 @@
 from django.contrib import admin
+from.models import Course, Lesson, Instructor, Learner, Question, Choice, Submission
+
+class ChoiceInline(admin.StackedInline):
+    model = Choice
+    extra = 2
+
+class QuestionInline(admin.StackedInline):
+    model = Question
+    extra = 2
+
+class QuestionAdmin(admin.ModelAdmin):
+    inlines = [ChoiceInline]
+    list_display = ['question_text', 'course']
+
+class LessonAdmin(admin.ModelAdmin):
+    list_display = ['title', 'course']
+
+admin.site.register(Course)
+admin.site.register(Lesson, LessonAdmin)
+admin.site.register(Instructor)
+admin.site.register(Learner)
+admin.site.register(Question, QuestionAdmin)
+admin.site.register(Choice)
+admin.site.register(Submission)from django.contrib import admin
 # <HINT> Import any new Models here
 from .models import Course, Lesson, Instructor, Learner
 
